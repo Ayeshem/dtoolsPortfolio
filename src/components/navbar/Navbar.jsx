19e2fadf6637191dadd1095dd4209c0cd32ac2a7
Ayesha Ikram <ikramayeshem@gmail.com>
2024-09-18 06:04:32 -0700

    links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,39 @@
+// import Sidebar from "../sidebar/Sidebar";
+// import "./navbar.scss";
+// import { motion } from "framer-motion";
+
+// const Navbar = () => {
+//   return (
+//     <div className="navbar">
+//       {/* Sidebar */}
+//       <Sidebar/>
+//       <div className="wrapper">
+//         <motion.span
+//           initial={{ opacity: 0, scale: 0.5 }}
+//           animate={{ opacity: 1, scale: 1 }}
+//           transition={{ duration: 0.5 }}
+//         >
+//           DTools Incorporation
+//         </motion.span>
+//         <div className="social">
+//           <a href="#">
+//             <img src="/instagram.png" alt="" />
+//           </a>
+//           {/* <a href="#">
+//             <img src="/instagram.png" alt="" />
+//           </a> */}
+//           <a href="#">
+//             <img src="/linkedin.png" alt="" />
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
@@ -16,14 +52,21 @@ const Navbar = () => {
           DTools Incorporation
         </motion.span>
         <div className="social">
-          <a href="#">
-            <img src="/facebook.png" alt="" />
+          {/* Instagram Link */}
+          <a 
+            href="https://www.instagram.com/dtools.incorporation?igsh=MWg3ejR0bW5ydnEwcQ==" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <img src="/instagram.png" alt="Instagram" />
           </a>
-          <a href="#">
-            <img src="/instagram.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="/youtube.png" alt="" />
+          {/* LinkedIn Link */}
+          <a 
+            href="https://www.linkedin.com/posts/d-tools-inc_ai-automation-futuretech-activity-7241433396780949504-Pkff?utm_source=share&utm_medium=member_ios" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <img src="/linkedin.png" alt="LinkedIn" />
           </a>
         </div>
       </div>
@@ -32,3 +75,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
