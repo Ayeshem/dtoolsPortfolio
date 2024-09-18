7cc7347ae281c23cfa6d6301c5915ef7bbaefc3c
Ayesha Ikram <ikramayeshem@gmail.com>
2024-09-18 04:45:02 -0700

    animation

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -5,18 +5,21 @@ import { motion, useScroll, useTransform } from "framer-motion";
 const Parallax = ({ type }) => {
   const ref = useRef();
 
+  // Track the scroll progress
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
 
-  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  // Move the entire background upwards as you scroll
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "-50%"]); // Adjust the percentage for desired speed
 
   return (
-    <div
+    <motion.div
       className="parallax"
       ref={ref}
       style={{
+        y: yBg, // Move the entire background as you scroll
         background:
           type === "services"
             ? "linear-gradient(180deg, #111132, #0c0c1d)"
@@ -26,18 +29,27 @@ const Parallax = ({ type }) => {
       <h1>
         {type === "services" ? "What We Do?" : "What We Did?"}
       </h1>
-      <motion.div className="mountains"></motion.div>
+      <motion.div
+        className="mountains"
+        style={{
+          backgroundImage: `url("/mountains.png")`,
+        }}
+      ></motion.div>
       <motion.div
         className="planets"
         style={{
-          y: yBg,
           backgroundImage: `url(${
             type === "services" ? "/planets.png" : "/sun.png"
           })`,
         }}
       ></motion.div>
-      <motion.div style={{ x: yBg }} className="stars"></motion.div>
-    </div>
+      <motion.div
+        className="stars"
+        style={{
+          backgroundImage: `url("/stars.png")`,
+        }}
+      ></motion.div>
+    </motion.div>
   );
 };
 
