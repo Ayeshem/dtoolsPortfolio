56335b4aa36f6862ea4b7196e031314f37d0eb08
Ayesha Ikram <ikramayeshem@gmail.com>
2024-09-19 10:38:33 -0700

    sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,47 @@
-import Sidebar from "../sidebar/Sidebar";
+// import Sidebar from "../sidebar/Sidebar";
+// import Links from "../sidebar/links/Links"; // Correct import path
+// import "./navbar.scss";
+// import { motion } from "framer-motion";
+
+// const Navbar = () => {
+//   return (
+//     <div className="navbar">
+//       {/* Sidebar */}
+//       <Sidebar/>
+//       <div className="wrapper">
+//         <motion.span
+//           initial={{ opacity: 0, scale: 0.5 }}
+//           animate={{ opacity: 1, scale: 1 }}
+//           transition={{ duration: 0.5 }}
+//         >
+//           DTools Incorporation
+//         </motion.span>
+//         <Links /> {/* Add the Links component */}
+//         <div className="social">
+//           {/* Instagram Link */}
+//           <a 
+//             href="https://www.instagram.com/dtools.incorporation?igsh=MWg3ejR0bW5ydnEwcQ==" 
+//             target="_blank" 
+//             rel="noopener noreferrer"
+//           >
+//             <img src="/instagram.png" alt="Instagram" />
+//           </a>
+//           {/* LinkedIn Link */}
+//           <a 
+//             href="https://www.linkedin.com/posts/d-tools-inc_ai-automation-futuretech-activity-7241433396780949504-Pkff?utm_source=share&utm_medium=member_ios" 
+//             target="_blank" 
+//             rel="noopener noreferrer"
+//           >
+//             <img src="/linkedin.png" alt="LinkedIn" />
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
 import Links from "../sidebar/links/Links"; // Correct import path
 import "./navbar.scss";
 import { motion } from "framer-motion";
@@ -6,8 +49,6 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
-      <Sidebar/>
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
@@ -41,6 +82,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
 
 
