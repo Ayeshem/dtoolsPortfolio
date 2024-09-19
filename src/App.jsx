5456d8fca42de78107e8251d55b20be9ad986b9d
Ayesha Ikram <ikramayeshem@gmail.com>
2024-09-19 08:24:31 -0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,7 @@ import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
-// const App = () => {
-//   return (
-//     <div>
-//       <section id="Homepage">
-//         <Navbar />
-//         <Hero />
-//       </section>
-//       <section id="Services">
-//         <Parallax type="services" />
-//       </section>
-//       <section>
-//         <Services />
-//       </section>
-//       <section id="Portfolio">
-//         <Parallax type="portfolio" />
-//       </section>
-//       <Portfolio />
-//       <section id="Contact">
-//         <Contact />
-//       </section>
-//       {/* Framer Motion Crash Course */}
-//       {/* <Test/>
-//     <Test/> */}
-//     </div>
-//   );
-// };
+
 const App = () => {
   return (
     <Router>
